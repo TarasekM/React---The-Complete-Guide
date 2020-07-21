bd88f1a5b194284d338aff56f49b3971c81d84f7
Mateusz Tarasek <m.tarasek@brainhint.com>
2020-07-21 16:11:56 +0200

    Make App a class and write some jsx to  render.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>First change on Page.</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>First change on Page.</h1>
+        <div>
+          <p>Just a paragraph.</p>
+          <p>And the second one.</p> 
+        </div>
+        <Person name="Davel" age="25" />
+        <Person name="King" age="30">(a true one)</Person>
+      </div>
+    )
+  }
 }
 
 export default App;
