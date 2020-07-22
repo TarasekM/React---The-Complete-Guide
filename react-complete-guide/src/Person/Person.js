fd8894567b15d6bbec935d05d2f372ea34b88973
Mateusz Tarasek <m.tarasek@brainhint.com>
2020-07-22 17:33:36 +0200

    Add css style to person.

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Person.css';
+
 const Person = (props) =>{
     return (
-    <div>
+    <div className="Person">
         <p id={props.id} onClick={props.click}>Hi person, I'm {props.name}{props.children}. {props.age} years young.</p>
         <input type="text" value={props.name} onChange={props.changed}/>
     </div>
