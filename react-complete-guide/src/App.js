7f9d6fbd24d44cc5fec3625efd3abe223f71ed04
Mateusz Tarasek <m.tarasek@brainhint.com>
2020-07-22 17:04:09 +0200

    Add a state with data to app. Implement method that generates UI form state data.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,7 +2,28 @@ import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component{
+  state = {
+    personList: [
+      {
+        "id": "0000-0000-0000-0000",
+        "name": "Mateusz",
+        "age": 25
+      },
+      {
+        "id": "0000-0000-0000-0001",
+        "name": "Jan",
+        "age": 27
+      },
+      {
+        "id": "0000-0000-0000-0002",
+        "name": "Tirion",
+        "age": 54
+      }
+    ]
+  }
+
   render(){
     return(
       <div className="App">
@@ -11,10 +32,30 @@ class App extends Component{
           <p>Just a paragraph.</p>
           <p>And the second one.</p> 
         </div>
-        <Person name="Davel" age="25" />
-        <Person name="King" age="30">(a true one)</Person>
+        <div>
+          <button onClick={this.switchNameHandler}>Switch the Name</button>
+          {this.getPersonList()}
+        </div>
       </div>
     )
+  }
+
+  switchNameHandler = () =>{
+    // console.log("was clicked");
+    this.setState({personList: [
+      {
+        "id": "0000-0000-0000-0000",
+        "name": "Davel",
+        "age": 24
+      }
+    ]})
+  };
+
+  getPersonList(){
+    const personList = this.state.personList.map(person => 
+      <Person key={person.id} name={person.name} age={person.age} />
+    );
+    return personList;
   }
 }
 
